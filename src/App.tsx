3212d63e02a589b847ca53d88f0bf0406ce25bca
at4 <at4@jc4.so-net.ne.jp>
2023-10-15 13:05:43 +0200

    Set basic structures

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-import logo from "./assets/logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
 import "./scss/App.scss";
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+export const App = () => {
+  const getPath = (scale: string, chord: string) => `/${scale}/${chord}`;
 
-export default App;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path={getPath(":scale", ":chord")} element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
