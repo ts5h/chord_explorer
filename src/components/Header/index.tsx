ab9a5f87620e2d862732565c78784c6ad9004e5f
at4 <at4@jc4.so-net.ne.jp>
2023-10-18 22:08:34 +0200

    Update the app name

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ export const Header: FC = () => {
     <HStack w="full" justify="center" bgColor="gray.400" color="white">
       <HStack w="full" maxW="1240px" align="end" px={5} py={2}>
         <Heading as="h1" size="xs" fontWeight="normal" lineHeight={1}>
-          <Link to="/c/major">Chord Player</Link>
+          <Link to="/c/major">Chord Explorer</Link>
         </Heading>
       </HStack>
     </HStack>
