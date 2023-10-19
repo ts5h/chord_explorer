a11835f7de349690f1ad3116c318b6ed6154a4d5
at4 <at4@jc4.so-net.ne.jp>
2023-10-19 21:09:09 +0200

    Add index to the scales

--- src/vo/Scales/index.ts
+++ src/vo/Scales/index.ts
@@ -1,24 +1,25 @@
 type Scale = {
+  index: number;
   label: string;
   value: string;
 };
 
 export const scales: Scale[] = [
-  { label: "C", value: "c" },
-  { label: "C#", value: "c-sharp" },
-  { label: "D♭", value: "d-flat" },
-  { label: "D", value: "d" },
-  { label: "D#", value: "d-sharp" },
-  { label: "E♭", value: "e-flat" },
-  { label: "E", value: "e" },
-  { label: "F", value: "f" },
-  { label: "F#", value: "f-sharp" },
-  { label: "G♭", value: "g-flat" },
-  { label: "G", value: "g" },
-  { label: "G#", value: "g-sharp" },
-  { label: "A♭", value: "a-flat" },
-  { label: "A", value: "a" },
-  { label: "A#", value: "a-sharp" },
-  { label: "B♭", value: "b-flat" },
-  { label: "B", value: "b" },
+  { index: 0, label: "C", value: "c" },
+  { index: 1, label: "C#", value: "c-sharp" },
+  { index: 1, label: "D♭", value: "d-flat" },
+  { index: 2, label: "D", value: "d" },
+  { index: 3, label: "D#", value: "d-sharp" },
+  { index: 3, label: "E♭", value: "e-flat" },
+  { index: 4, label: "E", value: "e" },
+  { index: 5, label: "F", value: "f" },
+  { index: 6, label: "F#", value: "f-sharp" },
+  { index: 6, label: "G♭", value: "g-flat" },
+  { index: 7, label: "G", value: "g" },
+  { index: 8, label: "G#", value: "g-sharp" },
+  { index: 8, label: "A♭", value: "a-flat" },
+  { index: 9, label: "A", value: "a" },
+  { index: 10, label: "A#", value: "a-sharp" },
+  { index: 10, label: "B♭", value: "b-flat" },
+  { index: 11, label: "B", value: "b" },
 ];
