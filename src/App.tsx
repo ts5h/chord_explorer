59c4651d07024f818f9cbde0465c58aa95e50a92
at4 <at4@jc4.so-net.ne.jp>
2023-10-25 22:57:10 +0200

    Add basename to the App

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export const App = () => {
   };
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />}>
           <Route path={getPath(":scale")} element={<Home />}>
