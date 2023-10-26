146c650abbc2c80b824c481a57d86c46c3272e10
at4 <at4@jc4.so-net.ne.jp>
2023-10-26 13:15:44 +0200

    Fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { Home } from "~/pages/Home";
 import "~/scss/App.scss";
 
 export const App = () => {
-  const getPath = (scale?: string, chord?: string) => {
-    if (scale && chord) return `/${scale}/${chord}`;
-    else if (scale) return `/${scale}`;
-    return "/";
-  };
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/:scale",
+      element: <Home />,
+    },
+    {
+      path: "/:scale/:chord",
+      element: <Home />,
+    },
+    {
+      path: "/*",
+      element: <Home />,
+    },
+  ]);
 
-  return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path={getPath(":scale")} element={<Home />}>
-            <Route path={getPath(":scale", ":chord")} element={<Home />} />
-          </Route>
-        </Route>
-
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 };
