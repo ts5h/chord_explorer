cf90f1f8b2b426cd532e8ce432fc91cbda64ca92
at4 <at4@jc4.so-net.ne.jp>
2025-12-06 22:45:35 +0100

    Update settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ts5h/",
+  base: "/chord_explorer/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
